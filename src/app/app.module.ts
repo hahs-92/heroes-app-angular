bf3b5fd0fd924a57afdc7f7bd22746232f445d09
hahs <alexandre_2420@outlook.com>
2022-07-26 09:51:03 -0500

    [added]: heroe service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 
+//http
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [AppRoutingModule, BrowserModule, BrowserAnimationsModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/heroes/pages/list-heroes/list-heroes.component.ts
+++ src/app/heroes/pages/list-heroes/list-heroes.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+//services
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-list-heroes',
   templateUrl: './list-heroes.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ListHeroesComponent implements OnInit {
+  heroes: Heroe[] = [];
 
-  constructor() { }
+  constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  getHeroes() {
+    this.heroesService.getHeroes().subscribe({
+      next: (resp) => {
+        this.heroes = resp;
+      },
+    });
+  }
 }
